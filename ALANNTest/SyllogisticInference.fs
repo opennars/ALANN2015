e5ec6b114a38d9951906b5f8949d880f9265ede8
TonyLo1 <tony_lofthouse@btinternet.com>
2015-07-31 20:40:51 +0100

    Syllogistic Inference Rules Added

---
+++ ALANNTest/SyllogisticInference.fs
@@ -0,0 +1,25 @@
+﻿module SyllogisticInference
+
+open Types
+open FirstOrderSyllogistic
+open HigherOrderSyllogistic
+
+let (|IsFirstOrder|_|) = 
+    function 
+    | Term(Inh, _), Term(Inh, _)
+    | Term(Inh, _), Term(Sim, _)
+    | Term(Sim, _), Term(Inh, _)
+    | Term(Sim, _), Term(Sim, _) -> Some() | _ -> None
+
+let (|IsHigherOrder|_|) = 
+    function 
+    | Term(Imp, _), Term(Imp, _)
+    | Term(Imp, _), Term(Equ, _)
+    | Term(Equ, _), Term(Imp, _)
+    | Term(Equ, _), Term(Equ, _) -> Some() | _ -> None
+
+let SyllogisticInference st1 st2 tv1 tv2 =
+    match st1, st2 with
+    | IsFirstOrder -> firstOrderSyllogisticInference st1 tv1 st2 tv2
+    | IsHigherOrder -> higherOrderSyllogisticInference st1 tv1 st2 tv2
+    | _ -> []
